7a8f9b265ecbe36609a82b66ea5cc07bb5ff312e
David Furnes <david@dfurnes.com>
2016-12-16 15:30:48 -0500

    Set locale by query string!

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SetLocale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        $locale = $request->query('locale');
+
+        if ($locale) {
+            app()->setLocale($locale);
+        }
+
+        return $next($request);
+    }
+}
