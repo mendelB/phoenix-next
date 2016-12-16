3fdcd170d47baa152b84223b7e2e8def8e4a4012
David Furnes <david@dfurnes.com>
2016-12-16 15:47:14 -0500

    A quick coat of paint on the index page.

--- resources/views/campaigns/index.blade.php
+++ resources/views/campaigns/index.blade.php
@@ -1,13 +1,33 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>Campaigns</h1>
-    <ul>
-        @foreach($campaigns as $campaign)
-            <li>
-                <h2><a href="{{ url('campaigns/'.$campaign->getSlug()) }}">{{ $campaign->getTitle() }}<a/></h2>
-                <p>{{ $campaign->getCallToAction() }}</p>
-            </li>
-        @endforeach
-    </ul>
+    <header role="banner" class="header">
+        <div class="wrapper">
+            <h1 class="header__title">Contentful Prototype</h1>
+            <p class="header__subtitle">Here's some campaigns.</p>
+        </div>
+    </header>
+
+    <div class="container -padded">
+        <div class="wrapper">
+            <div class="container__block">
+                <p>Check out these sample campaigns, powered by Contentful!</p>
+            </div>
+            <ul class="gallery -quartet">
+                @foreach($campaigns as $campaign)
+                    <li>
+                        <article class="tile">
+                            <a class="wrapper" href="{{ url('campaigns/'.$campaign->slug) }}">
+                                <div class="tile__meta">
+                                    <h1 class="tile__title">{{ $campaign->title }}</h1>
+                                    <p class="tile__tagline">{{ $campaign->callToAction }}</p>
+                                </div>
+                                <img alt="kitten overlords" src="{{ get_image_url($campaign->coverImage, 'square') }}" />
+                            </a>
+                        </article>
+                    </li>
+                @endforeach
+        </ul>
+        </div>
+    </div>
 @stop
