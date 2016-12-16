6848b126c1923092d25d25fa8a1565f9142d0ab2
David Furnes <david@dfurnes.com>
2016-12-16 12:00:21 -0500

    Add Forge.

--- resources/views/campaigns/show.blade.php
+++ resources/views/campaigns/show.blade.php
@@ -1,41 +1,61 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>{{ $campaign->title }}</h1>
-    <h2>{{ $campaign->callToAction }}</h2>
+    <header role="banner" class="header -hero" style="background-image: url('{{  get_image_url($campaign->coverImage, 'landscape') }}')">
+        <div class="wrapper">
+            <h1 class="header__title">{{ $campaign->title }}</h1>
+            <p class="header__subtitle">{{ $campaign->callToAction }}</p>
+        </div>
+    </header>
 
-    @if ($campaign->coverImage)
-        <img alt="{{ $campaign->coverImage->getTitle() }}" src="{{ get_image_url($campaign->coverImage, 'landscape') }}">
-    @endif
+    <div class="container -padded">
+        <div class="wrapper">
+            @if ($campaign->problemFact)
+                <div class="container__block">
+                    <h3>The Problem</h3>
+                    <dl>
+                        <dt>{{ $campaign->problemFact['content'] }}</dt>
+                        <dd><i>{{ $campaign->problemFact['source'] }}</i></dd>
+                    </dl>
+                </div>
+            @endif
 
-    @if ($campaign->problemFact)
-        <dl>
-            <dt>{{ $campaign->problemFact['content'] }}</dt>
-            <dd><i>{{ $campaign->problemFact['source'] }}</i></dd>
-        </dl>
-    @endif
+            @if ($campaign->solutionFact || $campaign->solutionStatement)
+                <div class="container__block">
+                    <h3>The Solution</h3>
+                    @if ($campaign->solutionFact)
+                        <dl>
+                            <dt>{{ $campaign->solutionFact['content'] }}</dt>
+                            <dd><i>{{ $campaign->solutionFact['source'] }}</i></dd>
+                        </dl>
+                    @endif
 
-    @if ($campaign->solutionFact)
-        <dl>
-            <dt>{{ $campaign->solutionFact['content'] }}</dt>
-            <dd><i>{{ $campaign->solutionFact['source'] }}</i></dd>
-        </dl>
-    @endif
+                    @if ($campaign->solutionStatement)
+                        <p>{{ $campaign->solutionStatement }}</p>
+                    @endif
+                </div>
+            @endif
 
-    @if ($campaign->solutionStatement)
-        <p>{{ $campaign->solutionStatement }}</p>
-    @endif
 
-    @if ($campaign->facts)
-        @foreach ($campaign->facts as $fact)
-            <dl>
-                <dt>{{ $fact['content'] }}</dt>
-                <dd>{{ $fact['source'] }}</dd>
-            </dl>
-        @endforeach
-    @endif
+            @if ($campaign->facts)
+                <div class="container__block">
+                    <h3>Facts</h3>
+                    @foreach ($campaign->facts as $fact)
+                        <dl>
+                            <dt>{{ $fact['content'] }}</dt>
+                            <dd class="footnote">{{ $fact['source'] }}</dd>
+                        </dl>
+                    @endforeach
+                </div>
+            @endif
 
-    @if ($campaign->faqs)
-        <div>{{ $campaign->faqs }}</div>
-    @endif
+            @if ($campaign->faqs)
+                <div class="container__block">
+                    <h3>FAQs</h3>
+                    <div>{{ $campaign->faqs }}</div>
+                </div>
+            @endif
+        </div>
+    </div>
+
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,16 +2,21 @@
 
 <html lang="en">
 
-  <head>
+<head>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>DoSomething.org</title>
-    <link rel="icon" type="image/ico" href="/favicon.ico?v1">
-    <link rel="stylesheet" href="">
-  </head>
 
-  <body>
-    @yield('content')
-  </body>
+    <link rel="icon" type="image/ico" href="/favicon.ico?v1">
+    <link rel="stylesheet" href="https://unpkg.com/@dosomething/forge@^6.7.4/dist/forge.css" media="screen, projection" type="text/css">
+</head>
+
+<body>
+<div class="chrome">
+    <div class="wrapper">
+        @yield('content')
+    </div>
+</div>
+</body>
 
 </html>
