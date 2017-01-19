3982757adf719e06c46882f09dfb22a1478a0d30
deadlybutter <thedeadlybutter@gmail.com>
2017-01-19 12:55:55 -0500

    Updating user model to have correct contracts and traits

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use DoSomething\Gateway\Contracts\NorthstarUserContract;
 use DoSomething\Gateway\Laravel\HasNorthstarToken;
 
-class User extends Authenticatable
+class User extends Model implements AuthenticatableContract, NorthstarUserContract
 {
     use Authenticatable, HasNorthstarToken;
+
+    protected $table = 'users';
 }
