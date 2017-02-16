783a68d67c760a0e49e0a981249464914d01cf3c
David Furnes <david@dfurnes.com>
2017-02-16 15:22:16 -0500

    Configure helper for client-side Markdown parsing.

--- resources/assets/helpers.js
+++ resources/assets/helpers.js
@@ -1,3 +1,5 @@
+import marked from 'marked';
+
 /**
  * Wait until the DOM is ready.
  *
@@ -8,5 +10,22 @@ export function ready(fn) {
     fn();
   } else {
     document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+/**
+ * Render Markdown for a React component.
+ *
+ * @param {String} source - Markdown source
+ * @returns {{__html}} - Prepared object for React's dangerouslySetInnerHtml
+ */
+export function markdown(source) {
+  // Markdown options <https://github.com/chjj/marked#options-1>
+  const options = {
+    sanitize: true
+  };
+
+  return {
+    __html: marked(source, options),
   }
 }
