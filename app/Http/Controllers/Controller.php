3f50b1fb1a018dfeb6dcb7a2fc3793ea0afb45d5
Diego Lorenzo <diego@diegolorenzo.com>
2017-02-16 13:56:08 -0500

    Removing method from Controller which is no longer needed.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,13 +10,4 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    /**
-     * Get the cache key for the current request.
-     *
-     * @return string
-     */
-    protected function makeCacheKeyFromRequest() {
-        return make_cache_key(request()->path(), request()->query());
-    }
 }
