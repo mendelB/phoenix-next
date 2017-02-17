52df258d49b1860c10c0aac8c07ec31839b4dc86
Diego Lorenzo <diego@diegolorenzo.com>
2017-02-17 14:52:40 -0500

    Expose service URLs from .env file for client JS.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var dotenv = require('dotenv').config();
 var configurator = require('@dosomething/webpack-config');
 
 var config = configurator({
@@ -9,5 +10,17 @@ var config = configurator({
 
 // Override output path.
 config.output.path = './public/dist';
+
+// Expose Service URLs
+config.plugins.push(
+  new webpack.DefinePlugin({
+    'services': {
+      'GLADIATOR_URL': JSON.stringify(process.env.GLADIATOR_URL),
+      'NORTHSTAR_URL': JSON.stringify(process.env.NORTHSTAR_URL),
+      'PHOENIX_URL': JSON.stringify(process.env.PHOENIX_URL),
+      'PHOENIX_LEGACY_URL': JSON.stringify(process.env.PHOENIX_LEGACY_URL),
+    }
+  })
+);
 
 module.exports = config;
