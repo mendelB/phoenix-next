6e9a8fc07a68d58788bcb816d655e489b7a58c98
David Furnes <david@dfurnes.com>
2017-02-28 11:04:08 -0500

    Set "tweet" appearance for short campaign updates.

---
+++ resources/assets/components/CampaignUpdateBlock/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import classnames from 'classnames';
+import { markdown } from '../../helpers';
+import Block from '../Block';
+import Figure from '../Figure';
+import DEFAULT_AVATAR from './default-avatar.png';
+import './campaign-update.scss';
+
+const Byline = ({author, jobTitle = 'DoSomething.org Staff', avatar = DEFAULT_AVATAR}) => (
+  <Figure image={avatar}>
+    <strong>{author}</strong><br/>
+    <p className="footnote">{jobTitle}</p>
+  </Figure>
+);
+
+const CampaignUpdateBlock = (props) => {
+  const { title, content, additionalContent } = props.fields;
+  let { author, jobTitle, avatar } = additionalContent;
+
+  const contentHtml = markdown(content);
+  const isTweet = content.length < 144;
+
+  return (
+    <Block>
+      { isTweet ? null : <h2>{title}</h2> }
+      <div className={classnames('campaign-update__content', {'-tweet': isTweet})}
+           dangerouslySetInnerHTML={contentHtml} />
+      { author ? <Byline author={author} jobTitle={jobTitle} avatar={avatar} />: null}
+    </Block>
+  );
+};
+
+export default CampaignUpdateBlock;
