e378df60b8fa31719ec4e237bc1472bbf08610b1
Diego Lorenzo <diego@diegolorenzo.com>
2017-03-07 15:14:00 -0500

    Adding Submissions actions and associated reducers.

--- resources/assets/reducers/submissions.js
+++ resources/assets/reducers/submissions.js
@@ -1,12 +1,31 @@
-import { STORED_REPORTBACK_SUBMISSION } from '../actions';
+import {
+  STORE_REPORTBACK_PENDING,
+  STORE_REPORTBACK_SUCESSFUL,
+  ADD_TO_SUBMISSIONS_LIST
+} from '../actions';
 
 /**
  * Submissions reducer:
  */
 const submissions = (state = {}, action) => {
   switch (action.type) {
-    case STORED_REPORTBACK_SUBMISSION:
-      return state;
+    case STORE_REPORTBACK_PENDING:
+      return Object.assign({}, state, {
+        isStoring: true
+      });
+
+    case STORE_REPORTBACK_SUCESSFUL:
+      return Object.assign({}, state, {
+        isStoring: false
+      });
+
+    case ADD_TO_SUBMISSIONS_LIST:
+      return Object.assign({}, state, {
+        data: [
+          ...state,
+          action.reportback
+        ]
+      })
 
     default:
       return state;
