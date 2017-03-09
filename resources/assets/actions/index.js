af0a12475f0daece2b133ece30d5a4641ea999af
Diego Lorenzo <diego@diegolorenzo.com>
2017-03-09 13:45:42 -0500

    Updating fetchReportbacks() method with new endpoint.

--- resources/assets/actions/index.js
+++ resources/assets/actions/index.js
@@ -61,7 +61,7 @@ export function fetchReportbacks(node, page) {
   return dispatch => {
     dispatch(requestingReportbacks(node));
 
-    return (new Phoenix).getAllReportbacks({ campaigns: node, page })
+    return (new Phoenix).get('reportbacks', { campaigns: node, page })
       .then(json => {
         dispatch(receivedReportbacks(node, page, json.data))
       })
