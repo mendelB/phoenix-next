38c0944eee4cf682b35fa10418a716fa8ef2c910
Diego Lorenzo <diego@diegolorenzo.com>
2017-03-15 15:17:59 -0400

    Moving NotFound component to components directory.

--- resources/assets/containers/App.js
+++ resources/assets/containers/App.js
@@ -8,9 +8,9 @@ import createBrowserHistory from 'history/lib/createBrowserHistory';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 
 import Chrome from './Chrome';
-import Activity from './Activity';
+import ActivityContainer from './ActivityContainer';
 import ContentPageContainer from './ContentPageContainer';
-import NotFound from './NotFound';
+import NotFound from '../components/NotFound';
 
 // Set the application "base name" to /campaigns/:slug so all pages are relative to that.
 const basename = window.location.pathname.split('/').slice(0, 3).join('/');
@@ -23,7 +23,7 @@ const App = (props) => (
   <Provider store={store}>
     <Router history={history}>
       <Route component={Chrome}>
-        <Route path="/" component={Activity}/>
+        <Route path="/" component={ActivityContainer}/>
         <Route path="/pages/:page" component={ContentPageContainer}/>
         <Route path='*' component={NotFound} />
       </Route>
