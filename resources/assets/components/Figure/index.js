72ed54b8a1af7189883111d6ff916f33c45b8271
David Furnes <david@dfurnes.com>
2017-03-21 13:48:31 -0400

    Add "BaseFigure" that allows any element to be media.

--- resources/assets/components/Figure/index.js
+++ resources/assets/components/Figure/index.js
@@ -3,21 +3,27 @@ import classnames from 'classnames';
 import { modifiers } from '../../helpers';
 import './figure.scss';
 
-const Figure = ({alignment, verticalAlignment, size, image, className, imageClassName, alt, children}) => (
+export const BaseFigure = ({alignment, verticalAlignment, media, size, className, children}) => (
   <article className={classnames('figure', className, modifiers(alignment, verticalAlignment, size))}>
-    <div className="figure__media">
-      <img className={imageClassName} alt={alt} src={image} />
-    </div>
-    <div className="figure__body">
-      {children}
-    </div>
+    <div className="figure__media">{media}</div>
+    <div className="figure__body">{children}</div>
   </article>
 );
 
-Figure.propTypes = {
+BaseFigure.propTypes = {
   alignment: React.PropTypes.oneOf(['left', 'right']),
   verticalAlignment: React.PropTypes.oneOf(['center']),
   size: React.PropTypes.oneOf(['small', 'medium', 'large']),
+  media: React.PropTypes.element,
+};
+
+export const Figure = (props) => {
+  const media = <img className={props.imageClassName} alt={props.alt} src={props.image} />;
+
+  return <BaseFigure {...props} media={media} />;
+};
+
+Figure.propTypes = {
   image: React.PropTypes.string.isRequired,
   alt: React.PropTypes.string,
 };
