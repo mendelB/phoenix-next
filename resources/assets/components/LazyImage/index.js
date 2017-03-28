27eae2986e5711d75c9df1e3054cb160b3d115f6
David Furnes <david@dfurnes.com>
2017-03-28 15:36:03 -0400

    Add LazyImage for lazy-loading and fading in images.

---
+++ resources/assets/components/LazyImage/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { EMPTY_IMAGE } from '../../helpers';
+
+class LazyImage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {loaded: false};
+  }
+
+  /**
+   * Perform actions immediately after mounting component.
+   */
+  componentDidMount() {
+    const loader = new Image();
+
+    // Load image and set `loaded: true` state when ready.
+    loader.onload = () => this.setState({ loaded: true });
+    if (this.props.src) loader.src = this.props.src;
+  }
+
+  /**
+   * Render the image.
+   *
+   * @returns {XML}
+   */
+  render() {
+    return <img {...this.props}
+                src={this.state.loaded && this.props.src ? this.props.src : EMPTY_IMAGE}
+                style={{opacity: this.state.loaded ? 1 : 0}} />;
+  }
+}
+
+LazyImage.propTypes = {
+  src: React.PropTypes.string,
+};
+
+export default LazyImage;
