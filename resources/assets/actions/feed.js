bea4f6a5d524dc5d3084198d8cbeb1c1f7f80eb7
David Furnes <david@dfurnes.com>
2017-03-31 14:36:21 -0400

    Pull out feed calculations into selectors.

--- resources/assets/actions/feed.js
+++ resources/assets/actions/feed.js
@@ -1,4 +1,5 @@
 import { FEED_INCREMENT_PAGE, fetchReportbacks } from '../actions';
+import { totalBlocksInFeed, totalReportbackBlocksInFeed, getBlockOffset } from '../selectors/feed';
 
 /**
  * Action Creators: these functions create actions, which describe changes
@@ -10,12 +11,28 @@ export function displayNextPage() {
   return {type: FEED_INCREMENT_PAGE };
 }
 
-// Action: user clicked the "view more" button.
+// Async Action: user clicked the "view more" button.
 export function clickedViewMore() {
-  return (dispatch, getState) => {
-    // @TODO: Only dispatch if we _need_ more reportbacks.
-    dispatch(fetchReportbacks());
-
+  return dispatch => {
     dispatch(displayNextPage());
+    dispatch(conditionallyFetchReportbacks());
+  }
+}
+
+// Async Action: determine if the feed needs to fetch more photos.
+export function conditionallyFetchReportbacks() {
+  return (dispatch, getState) => {
+    const state = getState();
+
+    // The number of needed reportbacks are the number of reportback blocks in the
+    // community feed + any additional offset that hasn't been filled by blocks.
+    const overflowReportbackBlocks = Math.max(0, getBlockOffset(state) - totalBlocksInFeed(state));
+    const neededReportbacks = totalReportbackBlocksInFeed(state) + overflowReportbackBlocks;
+
+    // Dispatch an HTTP request if we don't have enough reportbacks in the store.
+    if (state.reportbacks.ids.length < neededReportbacks) {
+      console.log(`Loading more reportbacks. We have ${state.reportbacks.ids.length} and need ${neededReportbacks}.`);
+      dispatch(fetchReportbacks());
+    }
   }
 }
--- resources/assets/containers/FeedContainer.js
+++ resources/assets/containers/FeedContainer.js
@@ -1,103 +1,14 @@
 import { connect } from 'react-redux';
 import Feed from '../components/Feed';
-import {
-  clickedViewMore,
-  clickedSignUp,
-} from '../actions';
-
-const BLOCKS_PER_ROW = 3;
-const ROWS_PER_PAGE = 3;
-
-/**
- * Return a new reportback block.
- *
- * @returns {Object}
- */
-const reportbackBlock = () => {
-  return {
-      id: 'dynamic',
-      type: 'customBlock',
-      fields: {
-        type: 'reportbacks',
-        displayOptions: ['one-third'],
-        additionalContent: { count: 1 }
-      }
-    };
-};
-
-/**
- * Map the given display option to a numeric point value.
- *
- * @param {Array} displayOption
- * @return int
- */
-const mapDisplayToPoints = (displayOption) => {
-  switch (displayOption[0]) {
-    case 'one-third': return 1;
-    case 'two-thirds': return 2;
-    case 'full': return 3;
-    default: return 0;
-  }
-};
-
-/**
- * Filter the blocks based on the page offset.
- *
- * @param blocks
- * @param offset
- */
-const filterVisibleBlocks = (blocks, offset) => {
-  const rowTarget = offset * ROWS_PER_PAGE;
-  const pointTarget = rowTarget * BLOCKS_PER_ROW;
-  let totalPoints = 0;
-
-  // Filter out blocks that don't fit within offset.
-  const filteredBlocks = blocks.filter(block => {
-    totalPoints += mapDisplayToPoints(block.fields.displayOptions);
-    return totalPoints < pointTarget;
-  });
-
-  // If we weren't able to fill enough rows with blocks, add
-  // additional reportback blocks until we hit the target.
-  while (totalPoints < pointTarget) {
-    filteredBlocks.push(reportbackBlock());
-    totalPoints++;
-  }
-
-  return filteredBlocks;
-};
-
-/**
- * Append reportback IDs to the reportback blocks.
- * @param reportbacks
- * @param blocks
- */
-const appendReportbacks = (reportbacks, blocks) => {
-  let reportbackIndex = 0;
-
-  const appendedBlocks = blocks.map(block => {
-    if (block.fields.type !== 'reportbacks') return block;
-
-    // Attach some unique reportback IDs to each block.
-    const count = block.fields.additionalContent.count || 3;
-    block.reportbacks = reportbacks.slice(reportbackIndex, reportbackIndex += count);
-
-    return block;
-  });
-
-  if (reportbackIndex > reportbacks.length) {
-    // @TODO: We need to dispatch an action here?
-  }
-
-  return appendedBlocks;
-};
+import { clickedViewMore, clickedSignUp } from '../actions';
+import { getBlocksWithReportbacks, getVisibleBlocks } from '../selectors/feed';
 
 /**
  * Provide state from the Redux store as props for this component.
  */
 const mapStateToProps = (state) => {
   return {
-    blocks: appendReportbacks(state.reportbacks.ids, filterVisibleBlocks(state.campaign.activityFeed, state.blocks.offset)),
+    blocks: getBlocksWithReportbacks(getVisibleBlocks(state), state.reportbacks.ids),
     callToAction: state.campaign.callToAction,
     submissions: state.submissions,
     signedUp: state.signups.data.includes(state.campaign.legacyCampaignId),
