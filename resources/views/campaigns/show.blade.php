4c34afb0fd9d3d0997970e1591383d86329176f2
Diego Lorenzo <diego@diegolorenzo.com>
2017-04-05 17:13:35 -0400

    Adding new partial for the lede banner on campaigns.

--- resources/views/campaigns/show.blade.php
+++ resources/views/campaigns/show.blade.php
@@ -1,12 +1,8 @@
 @extends('layouts.master')
 
 @section('content')
-    <header role="banner" class="header -hero" style="background-image: url('{{  get_image_url($campaign->coverImage, 'landscape') }}')">
-        <div class="wrapper">
-            <h1 class="header__title">{{ $campaign->title }}</h1>
-            <p class="header__subtitle">{{ $campaign->callToAction }}</p>
-        </div>
-    </header>
+
+    @include('partials.lede_banner')
 
     <div id="app">
         <div class="feed-enclosure">
@@ -21,4 +17,5 @@
             </div>
         </div>
     </div>
+
 @endsection
