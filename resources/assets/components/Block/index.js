63591c1cbf7234ddaff21c3e0616d2419df5804c
David Furnes <david@dfurnes.com>
2017-04-07 15:24:31 -0400

    Move "block title" in with the main block component.

--- resources/assets/components/Block/index.js
+++ resources/assets/components/Block/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import classNames from 'classnames';
 import './block.scss';
 
-const Block = (props) => {
-  return (
-    <div className={classNames('block', props.className)}>
-      {props.children}
-    </div>
-  );
+export const BlockTitle = ({ children }) => (
+  <h4 className="block__title">{children}</h4>
+);
+
+BlockTitle.PropTypes = {
+  children: React.PropTypes.string,
 };
+
+export const Block = (props) => (
+  <div className={classNames('block', props.className)}>
+    {props.children}
+  </div>
+);
 
 export default Block;
