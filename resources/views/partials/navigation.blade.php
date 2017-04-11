f77ca29a50ac11146b7e1a55afb78783c3ded53b
David Furnes <david@dfurnes.com>
2017-04-11 17:13:07 -0400

    Add query string with destination title for campaigns.

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -25,7 +25,7 @@
                         <li><a href="{{ url('next/logout') }}" class="secondary-nav-item" id="link--logout">Log Out</a></li>
                     </ul>
                 @else
-                    <a href="{{ url('next/login') }}">Log In</a>
+                    <a href="{{ route('login', isset($campaign) ? ['destination' => $campaign->title] : null) }}">Log In</a>
                 @endif
             </li>
         </ul>
