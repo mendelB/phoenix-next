305232ef7acb0ec5be1a926d13b4ac467b50a7b6
David Furnes <david@dfurnes.com>
2017-04-11 17:15:07 -0400

    Fix issue where campaign index would set destination as first campaign.
    Why is the iterator value accessible outside foreach block in PHP?!

--- resources/views/campaigns/index.blade.php
+++ resources/views/campaigns/index.blade.php
@@ -9,15 +9,15 @@
                 web experience.</p>
             </div>
             <ul class="gallery -quartet">
-                @foreach($campaigns as $campaign)
+                @foreach($campaigns as $link)
                     <li>
                         <article class="tile">
-                            <a class="wrapper" href="{{ url('campaigns/'.$campaign->slug) }}">
+                            <a class="wrapper" href="{{ url('campaigns/'.$link->slug) }}">
                                 <div class="tile__meta">
-                                    <h1 class="tile__title">{{ $campaign->title }}</h1>
-                                    <p class="tile__tagline">{{ $campaign->callToAction }}</p>
+                                    <h1 class="tile__title">{{ $link->title }}</h1>
+                                    <p class="tile__tagline">{{ $link->callToAction }}</p>
                                 </div>
-                                <img alt="kitten overlords" src="{{ get_image_url($campaign->coverImage, 'square') }}" />
+                                <img alt="kitten overlords" src="{{ get_image_url($link->coverImage, 'square') }}" />
                             </a>
                         </article>
                     </li>
