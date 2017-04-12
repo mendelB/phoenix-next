034ff97fab3623cff0f063a6b7a1f8af01847dd5
Diego Lorenzo <diego@diegolorenzo.com>
2017-04-12 13:25:11 -0400

    Updates to styles and structure for campaign content pages.

--- resources/assets/components/ContentPage/index.js
+++ resources/assets/components/ContentPage/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import Block from '../Block';
 import Markdown from '../Markdown';
+import CallToActionContainer from '../../containers/CallToActionContainer';
 import { Flex, FlexCell } from '../Flex';
+
+import './content-page.scss';
 
 const ContentPage = ({ pages, route }) => {
   const page = pages.find(page => page.fields.slug === route.page);
 
+  const ctaContent = 'some content that needs to get output';
+
   return (
-    <Flex>
-      <FlexCell>
-        <Block>
-          <h2>{page.fields.title}</h2>
-          <Markdown>{page.fields.content}</Markdown>
-        </Block>
-      </FlexCell>
-    </Flex>
+    <div className="content-page">
+      <article>
+        <h2>{page.fields.title}</h2>
+        <Markdown>{page.fields.content}</Markdown>
+      </article>
+      <CallToActionContainer fields={ {title: 'something', content: ctaContent} } />
+    </div>
   );
 };
 
