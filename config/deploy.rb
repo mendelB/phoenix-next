01a13193e02916b5ef41ede2085d7f8f624b358d
blisteringherb <morgan@solosuenos.com>
2017-04-13 13:50:40 -0400

    Adding missing step to trigger restarting php

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,4 +53,5 @@ end
 after "deploy:update", "deploy:cleanup"
 after "deploy:symlink", "deploy:link_folders"
 after "deploy:link_folders", "deploy:artisan_migrate"
+after "deploy:artisan_migrate", "deploy:artisan_cache_clear"
 after "deploy:artisan_cache_clear", "deploy:restart_php"
