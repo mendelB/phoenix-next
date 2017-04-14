3b3ff42dc097ff0c63012c17577f54483651e6fd
David Furnes <david@dfurnes.com>
2017-04-14 17:35:59 -0400

    Use provider icon as backup for embed images.

--- resources/assets/components/Embed/index.js
+++ resources/assets/components/Embed/index.js
@@ -23,10 +23,10 @@ class Embed extends React.Component {
     // If an <iframe> code snippet is provided, use that. Otherwise, build preview card.
     if (this.state.code) {
       embed = <div className="media-video" dangerouslySetInnerHTML={{__html: this.state.code }} />;
-    } else if (this.state.title && this.state.url && this.state.image) {
+    } else if (this.state.title && this.state.url) {
       embed = (
         <a href={this.state.url} target="_blank">
-          <Figure className="embed__preview" image={this.state.image} alignment="left" size="large">
+          <Figure className="embed__preview" image={this.state.image || this.state.provider.icon} alignment="left-collapse" size="large">
             <h3>{ this.state.title }</h3>
             { this.state.description ? <p>{ this.state.description }</p> : null }
             <p className="footnote">{ this.state.provider.name }</p>
