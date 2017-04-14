783eb5b8e16df87ec124e87e73e56302e96752fd
Joe Kent <thedeadlybutter@gmail.com>
2017-04-14 11:09:27 -0400

    Analytics fix (#192)
    * No stats for admins
    
    * More renaming

--- resources/assets/actions/analytics.js
+++ resources/assets/actions/analytics.js
@@ -4,11 +4,18 @@ import { analyze } from '@dosomething/analytics';
 // Action: Track a custom event
 export function trackEvent(collection, metadata) {
   return (dispatch, getState) => {
-    const state = transformState(collection, getState());
+    const appState = getState();
+
+    // Don't track admins on production!
+    if (appState.user.role === 'admin' && process.env.NODE_ENV === 'production') {
+      return;
+    }
+
+    const transformedState = transformState(collection, appState);
 
     analyze(collection, {
       metadata,
-      state,
+      transformedState,
     });
   }
 }
