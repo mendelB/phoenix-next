2188de547844958828cb4408802fc35a2429d3f9
David Furnes <david@dfurnes.com>
2017-04-19 11:15:57 -0400

    Style updates.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const configure = require('@dosomething/webpack-config');
+const path = require('path');
 
 // Load any environment variables from `.env`.
 require('dotenv').config();
@@ -7,24 +8,24 @@ require('dotenv').config();
 // Configure Webpack using `@dosomething/webpack-config`.
 module.exports = configure({
   entry: {
-    app: './resources/assets/init.js'
+    app: './resources/assets/init.js',
   },
   output: {
     // Override output path for Laravel's "public" directory.
-    path:__dirname + '/public/next/assets',
+    path: path.join(__dirname, '/public/next/assets'),
   },
 
   plugins: [
     // Inline Service URLs into the build.
     new webpack.DefinePlugin({
-      'services': {
-        'GLADIATOR_URL': JSON.stringify(process.env.GLADIATOR_URL) || null,
-        'NORTHSTAR_URL': JSON.stringify(process.env.NORTHSTAR_URL) || null,
-        'PHOENIX_URL': JSON.stringify(process.env.PHOENIX_URL) || null,
-        'PHOENIX_LEGACY_URL': JSON.stringify(process.env.PHOENIX_LEGACY_URL) || null,
-        'KEEN_PROJECT_ID': JSON.stringify(process.env.KEEN_PROJECT_ID) || null,
-        'KEEN_WRITE_KEY': JSON.stringify(process.env.KEEN_WRITE_KEY) || null,
-      }
-    })
-  ]
+      services: {
+        GLADIATOR_URL: JSON.stringify(process.env.GLADIATOR_URL) || null,
+        NORTHSTAR_URL: JSON.stringify(process.env.NORTHSTAR_URL) || null,
+        PHOENIX_URL: JSON.stringify(process.env.PHOENIX_URL) || null,
+        PHOENIX_LEGACY_URL: JSON.stringify(process.env.PHOENIX_LEGACY_URL) || null,
+        KEEN_PROJECT_ID: JSON.stringify(process.env.KEEN_PROJECT_ID) || null,
+        KEEN_WRITE_KEY: JSON.stringify(process.env.KEEN_WRITE_KEY) || null,
+      },
+    }),
+  ],
 });
