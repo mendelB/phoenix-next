134de2c52d9282636634f4da4c016422d409ae09
Diego Lorenzo <diego@diegolorenzo.com>
2017-04-27 13:41:22 -0400

    Adding code comments.

--- resources/assets/helpers/test.js
+++ resources/assets/helpers/test.js
@@ -4,16 +4,19 @@ import {
   contentfulImageUrl,
 } from './index';
 
+// Test contentfulImageUrl()
 test('generate contentful image url with added parameters', () => {
   const contentfulImage = contentfulImageUrl('//images.contentful.com/somecrazystring.jpg', 800, 600, 'fill');
 
   expect(contentfulImage).toBe('//images.contentful.com/somecrazystring.jpg?w=800&h=600&fit=fill');
 });
 
+// Test makeHash()
 test('makes hash from a string', () => {
   expect(makeHash('some arbitrary string here')).toBe(988941499);
 });
 
+// Test modifiers()
 test('prefix a class name', () => {
   expect(modifiers('danger')).toEqual(expect.arrayContaining(['-danger']));
 });
