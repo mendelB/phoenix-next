7e3b7603facd9c5957cf69afa4be35abdaa6421c
Diego Lorenzo <diego@diegolorenzo.com>
2017-04-27 14:38:25 -0400

    Updating tests some more.

--- resources/assets/helpers/test.js
+++ resources/assets/helpers/test.js
@@ -4,7 +4,9 @@ import {
   contentfulImageUrl,
 } from './index';
 
-// Test contentfulImageUrl()
+/**
+ * Test contentfulImageUrl()
+ */
 test('generate contentful image url with defaults', () => {
   const contentfulImage = contentfulImageUrl('//images.contentful.com/somecrazystring.jpg');
 
@@ -23,12 +25,25 @@ test('generate contentful image url with all specified parameters', () => {
   expect(contentfulImage).toBe('//images.contentful.com/somecrazystring.jpg?w=800&h=600&fit=fill');
 });
 
-// Test makeHash()
-test('makes hash from a string', () => {
-  expect(makeHash('some arbitrary string here')).toBe(988941499);
+/**
+ * Test makeHash()
+ */
+test('hashing the same string gives the same result', () => {
+  expect(makeHash('some arbitrary string here')).toBe(makeHash('some arbitrary string here'));
 });
 
-// Test modifiers()
+test('hashing a different string gives a different result', () => {
+  expect(makeHash('some arbitrary string here')).not.toBe(makeHash('some other arbitrary string here'));
+});
+
+test('cannot hash undefined or null value', () => {
+  expect(() => makeHash(undefined)).toThrow();
+  expect(() => makeHash(null)).toThrow();
+});
+
+/**
+ * Test modifiers()
+ */
 test('prefix a class name', () => {
   expect(modifiers('danger')).toEqual(expect.arrayContaining(['-danger']));
 });
@@ -37,6 +52,6 @@ test('prefix a series of class names', () => {
   expect(modifiers('danger', 'will', 'robinson')).toEqual(expect.arrayContaining(['-danger', '-will', '-robinson']));
 });
 
-test('prefix a series of class names, ignoring null or undefined values', () => {
+test('prefix a series of class names, ignoring undefined or null values', () => {
   expect(modifiers('danger', undefined, 'will', null, 'robinson')).toEqual(expect.arrayContaining(['-danger', '-will', '-robinson']));
 });
