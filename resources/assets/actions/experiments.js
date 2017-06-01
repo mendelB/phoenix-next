b2b90a815b9e8a017a0591998dff37f031e935b3
Diego Lorenzo <diego@diegolorenzo.com>
2017-06-01 10:12:32 -0400

    Update to remove signup requirement for experiment conversion.

--- resources/assets/actions/experiments.js
+++ resources/assets/actions/experiments.js
@@ -3,16 +3,10 @@ import {
   ADD_TO_EXPERIMENTS_STORE,
   UPDATE_EXPERIMENTS_STORE,
   CONVERT_EXPERIMENT,
-  queueEvent,
 } from '../actions';
 
 export function convertExperiment(name) {
-  return (dispatch, getState) => {
-    if (! getState().user.id) {
-      dispatch(queueEvent('convertExperiment', name));
-      return;
-    }
-
+  return (dispatch) => {
     dispatch({
       type: CONVERT_EXPERIMENT,
       name,
