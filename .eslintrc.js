81bc6d7f6c636a99bd05e9841da583ae8fe61916
David Furnes <david@dfurnes.com>
2017-06-12 17:24:18 -0400

    Require mult-line curly braces.

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,9 @@ module.exports = {
     // For now, only warn on missing alt or caption.
     'jsx-a11y/alt-text': 'warn',
     'jsx-a11y/media-has-caption': 'warn',
+
+    // Require multi-line curly braces for all conditionals.
+    'curly': ['error', 'all'],
+    'brace-style': ['error', '1tbs', { allowSingleLine: false }],
   }
 };
