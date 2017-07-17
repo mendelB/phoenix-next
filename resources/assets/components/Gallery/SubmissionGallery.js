2ca8a2e85f92dd27d1ad89ef9c9c4b0e883745df
Diego Lorenzo <diego@diegolorenzo.com>
2017-07-17 15:45:36 -0400

    Updating to switch fetching User RBs from Uploader to Submission Gallery.

--- resources/assets/components/Gallery/SubmissionGallery.js
+++ resources/assets/components/Gallery/SubmissionGallery.js
@@ -4,30 +4,39 @@ import Gallery from './Gallery';
 import ReportbackItem from '../ReportbackItem';
 import { makeHash } from '../../helpers';
 
-const renderReportbackItem = (submission) => {
-  // @TODO: Normalize data for uploaded RBs vs API retrieved RBs if possible...
-  const key = makeHash(submission.media.uri || submission.media.filePreviewUrl);
-  const url = submission.media.uri || submission.media.filePreviewUrl;
+class SubmissionGallery extends React.Component {
+  static renderReportbackItem(submission) {
+    // @TODO: Normalize data for uploaded RBs vs API retrieved RBs if possible...
+    const key = makeHash(submission.media.uri || submission.media.filePreviewUrl);
+    const url = submission.media.uri || submission.media.filePreviewUrl;
 
-  return <ReportbackItem key={key} {...submission} url={url} reaction={null} basicDisplay />;
-};
+    return <ReportbackItem key={key} {...submission} url={url} reaction={null} basicDisplay />;
+  }
 
-const SubmissionGallery = ({ submissions }) => {
-  const { isFetching = false, items } = submissions;
+  componentDidMount() {
+    this.props.fetchUserReportbacks(this.props.userId, this.props.legacyCampaignId);
+  }
 
+  render() {
+    const isFetching = this.props.submissions.isFetching;
+    const items = this.props.submissions.items;
 
-  return isFetching
-    ? <div className="spinner -centered" />
-    : <Gallery type="triad">
-      {items.map(submission => renderReportbackItem(submission))}
-    </Gallery>;
-};
+    return isFetching
+      ? <div className="spinner -centered" />
+      : <Gallery type="triad">
+        {items.map(submission => SubmissionGallery.renderReportbackItem(submission))}
+      </Gallery>;
+  }
+}
 
 SubmissionGallery.propTypes = {
+  fetchUserReportbacks: PropTypes.func.isRequired,
+  legacyCampaignId: PropTypes.string.isRequired,
   submissions: PropTypes.shape({
     isFetching: PropTypes.bool,
     items: PropTypes.array,
   }).isRequired,
+  userId: PropTypes.string.isRequired,
 };
 
 export default SubmissionGallery;
--- resources/assets/components/Gallery/SubmissionGalleryContainer.js
+++ resources/assets/components/Gallery/SubmissionGalleryContainer.js
@@ -1,14 +1,25 @@
 import { connect } from 'react-redux';
 import SubmissionGallery from './SubmissionGallery';
+import { fetchUserReportbacks } from '../../actions';
 
 /**
  * Provide state from the Redux store as props for this component.
  */
 const mapStateToProps = state => ({
+  legacyCampaignId: state.campaign.legacyCampaignId,
   submissions: state.submissions,
+  userId: state.user.id,
 });
+
+/**
+ * Provide pre-bound functions that allow the component to dispatch
+ * actions to the Redux store as props for this component.
+ */
+const mapDispatchToProps = {
+  fetchUserReportbacks,
+};
 
 /**
  * Export the container component.
  */
-export default connect(mapStateToProps)(SubmissionGallery);
+export default connect(mapStateToProps, mapDispatchToProps)(SubmissionGallery);
