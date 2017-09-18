6e568c22f64dfbfde907b3068650caebbe49b461
Diego Lorenzo <diego@diegolorenzo.com>
2017-09-18 10:49:16 -0400

    Simplifying the App component to only deal with showing campaign or landing pages.

--- resources/assets/components/App.js
+++ resources/assets/components/App.js
@@ -1,61 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { get } from 'lodash';
 import { Provider } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 
+import Page from './Page';
+import { CampaignPageContainer } from './Page';
+import { LandingPageContainer } from './Page';
+
 import { initializeStore } from '../store';
-import { isCampaignClosed } from '../helpers';
-import { paths } from '../helpers/navigation';
-import ChromeContainer from '../containers/ChromeContainer';
-import { FeedContainer } from './Feed';
-import { ActionPageContainer } from './ActionPage';
-import { BlockContainer } from './Block';
-import { QuizContainer } from './Quiz';
-import ContentPageContainer from '../containers/ContentPageContainer';
-import NotFound from './NotFound';
-
-const wrap = (Container, Component) => props => (
-  <Container><Component {...props} /></Container>
-);
-
-const chrome = component => wrap(ChromeContainer, component);
 
 const App = ({ store, history }) => {
   initializeStore(store);
 
-  const endDate = get(store.getState().campaign.endDate, 'date', null);
-  const actionPage = isCampaignClosed(endDate) ? null : (
-    <Route path={paths.action} component={chrome(ActionPageContainer)} />
-  );
+  const isAffiliated = store.getState().signups.thisCampaign;
+  const useLandingPage = store.getState().campaign.landingPage ? true : false;
 
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
         <Switch>
-          {/* Base user experience */}
-          <Route path={paths.community} exact component={chrome(FeedContainer)} />
-          { actionPage }
-          <Route path={`${paths.pages}:page`} component={chrome(ContentPageContainer)} />
-          <Route path={`${paths.blocks}:id`} component={chrome(BlockContainer)} />
-          <Route path={`${paths.quiz}:slug`} component={chrome(QuizContainer)} />
-          {/* * */}
-
-          {/* Custom user experiences... */}
-
-          {/* 404 default */}
-          <Route component={chrome(NotFound)} />
-          {/* * */}
+          { (! isAffiliated  && useLandingPage) ?
+            <Route path="/us/campaigns/:slug" component={LandingPageContainer} />
+            :
+            <Route path="/us/campaigns/:slug" component={CampaignPageContainer} />
+          }
         </Switch>
       </ConnectedRouter>
     </Provider>
   );
-};
-
-App.propTypes = {
-  store: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-  history: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
 };
 
 export default App;
