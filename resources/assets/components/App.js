9cd36ec94c1b41c6b13c1fafc910c93027d2f232
Diego Lorenzo <diego@diegolorenzo.com>
2017-09-18 16:36:25 -0400

    Adding new Campaign component to handle page logic.

--- resources/assets/components/App.js
+++ resources/assets/components/App.js
@@ -1,29 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
-import { Switch, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 
-import { CampaignPageContainer, LandingPageContainer } from './Page';
-
+import { CampaignContainer } from './Campaign';
 import { initializeStore } from '../store';
 
 const App = ({ store, history }) => {
   initializeStore(store);
 
-  const isAffiliated = store.getState().signups.thisCampaign;
-  const useLandingPage = store.getState().campaign.landingPage !== null;
-
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        <Switch>
-          { (! isAffiliated && useLandingPage) ?
-            <Route path="/us/campaigns/:slug" component={LandingPageContainer} />
-            :
-            <Route path="/us/campaigns/:slug" component={CampaignPageContainer} />
-          }
-        </Switch>
+        <Route path="/us/campaigns/:slug" component={CampaignContainer} />
       </ConnectedRouter>
     </Provider>
   );
---
+++ resources/assets/components/Campaign/CampaignContainer.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+
+import Campaign from './Campaign';
+
+const mapStateToProps = state => ({
+  isAffiliated: state.signups.thisCampaign,
+  useLandingPage: state.campaign.landingPage !== null,
+});
+
+// const mapActionsToProps = {
+
+// };
+
+export default connect(mapStateToProps)(Campaign);
