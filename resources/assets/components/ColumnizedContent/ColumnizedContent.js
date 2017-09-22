fbebda6f2fdb5b295512daa4c9c96e81cf60a8b6
Diego Lorenzo <diego@diegolorenzo.com>
2017-09-22 11:03:50 -0400

    Adding index as key so React is not angry.

--- resources/assets/components/ColumnizedContent/ColumnizedContent.js
+++ resources/assets/components/ColumnizedContent/ColumnizedContent.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-array-index-key */
+
 import React from 'react';
 import { chunk } from 'lodash';
 import PropTypes from 'prop-types';
@@ -11,7 +13,11 @@ const ColumnizedContent = ({ content, columnCount = 2, className = null }) => {
 
   return (
     <div>
-      { columns.map(column => (<div className={classnames(className)}>{column}</div>))}
+      { columns.map((column, index) => (
+        <div key={index} className={classnames(className)}>
+          {column}
+        </div>
+      ))}
     </div>
   );
 };
