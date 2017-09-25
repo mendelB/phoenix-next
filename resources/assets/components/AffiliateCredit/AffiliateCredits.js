4f0167235b0dd5a0db9d7e0a09909bf70dd39701
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-09-25 12:18:52 -0400

    adding component to deal with multiple affiliates - partner and sponsor

---
+++ resources/assets/components/AffiliateCredit/AffiliateCredits.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import AffiliateCredit from './AffiliateCredit';
+
+const AffiliateCredits = ({ affiliatePartners, affiliateSponsors }) => {
+  const sponsor = affiliateSponsors[0];
+  const partner = affiliatePartners[0];
+
+  const sponsorCredit = sponsor ?
+    <AffiliateCredit link={sponsor.fields.link} title={sponsor.fields.title} />
+    : null;
+  const partnerCredit = (partner ?
+    <AffiliateCredit link={partner.fields.link} title={partner.fields.title} />
+    : null);
+
+  const multipleCredits = sponsor && partner ? ' and ' : null;
+
+  return (
+    (sponsorCredit || partnerCredit) ?
+      <span>In partnership with {sponsorCredit} {multipleCredits} {partnerCredit}</span>
+      : null
+  );
+};
+
+AffiliateCredits.propTypes = {
+  affiliateSponsors: PropTypes.array.isRequired, // eslint-disable-line react/forbid-prop-types
+  affiliatePartners: PropTypes.array.isRequired, // eslint-disable-line react/forbid-prop-types
+};
+
+export default AffiliateCredits;
