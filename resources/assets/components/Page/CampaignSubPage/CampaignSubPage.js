dc4db6fa65fb519e064a56294ddbc2af96492c68
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-09-27 17:57:43 -0400

    hiding CTAs when campaign is past endate

--- resources/assets/components/Page/CampaignSubPage/CampaignSubPage.js
+++ resources/assets/components/Page/CampaignSubPage/CampaignSubPage.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import { find } from 'lodash';
+import { find, get } from 'lodash';
 import PropTypes from 'prop-types';
 
 import NotFound from '../../NotFound';
 import Markdown from '../../Markdown';
 import ScrollConcierge from '../../ScrollConcierge';
 import CallToActionBlockContainer from '../../../containers/CallToActionBlockContainer';
+import { isCampaignClosed } from '../../../helpers';
 
 import './campaign-subpage.scss';
 
 const CampaignSubPage = (props) => {
-  const { match, noun, pages, tagline, verb } = props;
+  const { match, noun, pages, tagline, verb, endDate } = props;
 
   const subPage = find(pages, page => page.fields.slug === match.params.slug);
 
   if (! subPage) {
     return <NotFound />;
   }
+
+  const isClosed = isCampaignClosed(get(endDate, 'date', null));
 
   const ctaContent = `${tagline}\n\n__Join hundreds of members and ${verb.plural} ${noun.plural}!__`;
 
@@ -31,17 +34,21 @@ const CampaignSubPage = (props) => {
         </article>
       </div>
 
-      <div className="secondary">
-        <CallToActionBlockContainer
-          fields={{ content: ctaContent }}
-          modifierClasses="dark-bg"
-        />
-      </div>
+      { ! isClosed ? (
+        <span>
+          <div className="secondary">
+            <CallToActionBlockContainer
+              fields={{ content: ctaContent }}
+              modifierClasses="dark-bg"
+            />
+          </div>
 
-      <CallToActionBlockContainer
-        fields={{ title: tagline }}
-        modifierClasses="transparent border-none"
-      />
+          <CallToActionBlockContainer
+            fields={{ title: tagline }}
+            modifierClasses="transparent border-none"
+          />
+        </span>
+      ) : null }
     </div>
   );
 };
@@ -59,6 +66,11 @@ CampaignSubPage.propTypes = {
       content: PropTypes.string.isRequired,
     }),
   })),
+  endDate: PropTypes.shape({
+    date: PropTypes.string,
+    timezone: PropTypes.string,
+    timezone_type: PropTypes.number,
+  }),
   tagline: PropTypes.string,
   verb: PropTypes.shape({
     singular: PropTypes.string,
@@ -72,6 +84,7 @@ CampaignSubPage.defaultProps = {
     singular: 'action',
     plural: 'action',
   },
+  endDate: null,
   tagline: 'Ready to start?',
   verb: {
     singular: 'take',
