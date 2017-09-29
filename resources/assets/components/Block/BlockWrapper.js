034a9cb932a66fd343fae433685e2a8de2f540e2
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-09-29 16:34:53 -0400

    fix proptypes

--- resources/assets/components/Block/BlockWrapper.js
+++ resources/assets/components/Block/BlockWrapper.js
@@ -5,16 +5,21 @@ import classnames from 'classnames';
 import { Link } from 'react-router-dom';
 import './block-wrapper.scss';
 
-type BlockTitleProps = {
-  shareLink: ?string,
-  title: ?string,
+type BlockTitleLinkProps = {
+  shareLink: string,
+  title: string,
 };
 
-const BlockTitleLink = ({ title, shareLink }: BlockTitleProps) => (
+const BlockTitleLink = ({ title, shareLink }: BlockTitleLinkProps) => (
   shareLink.match(/http/) ?
     <a href={shareLink}>{title}</a>
     : <Link to={shareLink}>{title}</Link>
 );
+
+type BlockTitleProps = {
+  shareLink: ?string,
+  title: ?string,
+};
 
 const BlockTitle = ({ title, shareLink }: BlockTitleProps) => {
   const titleElement = shareLink ? <BlockTitleLink title={title} shareLink={shareLink} /> : title;
