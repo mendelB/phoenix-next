03a594c902a5420e39f6fac85248cf6a664a51d2
Shae Smith <sbsmith86@gmail.com>
2017-10-05 10:12:10 -0400

    toggle visibility of opt-in details

--- resources/assets/components/AffiliateOption/AffiliateOption.js
+++ resources/assets/components/AffiliateOption/AffiliateOption.js
@@ -1,5 +1,9 @@
+/* global document */
+
 import React from 'react';
 import PropTypes from 'prop-types';
+import './affiliateOption.scss';
+
 
 const AffiliateOption = (props) => {
   const { optedOut, clickedOptOut } = props;
@@ -7,18 +11,21 @@ const AffiliateOption = (props) => {
   const handleClick = (event) => {
     event.preventDefault();
     console.log('clicking');
+    const details = document.querySelector('.footnote-details');
+
+    details.classList.toggle('js-footnote-hidden');
   };
 
   return (
-    <div className="form-wrapper">
-      <label className="option -checkbox" htmlFor="opt_in">
-        <input type="checkbox" id="opt_in" name="affiliate_messaging_opt_in" value={optedOut} className="form-checkbox" onClick={clickedOptOut} />
+    <div className="form-wrapper affiliate-option">
+      <label className="option -checkbox" htmlFor="affiliate_opt_in">
+        <input type="checkbox" id="opt_in" name="affiliate_opt_in" value={optedOut} className="form-checkbox" onClick={clickedOptOut} />
         <span className="option__indicator" />
         Also sign me up for messages from our partner
       </label>
       <div className="footnote">
-        <button className="button -tertiary" onClick={handleClick}>Affiliate Opt-in More Information Label</button>
-        <div className="js-footnote-hidden">
+        <button onClick={handleClick}>Affiliate Opt-in More Information Label</button>
+        <div className="footnote-details js-footnote-hidden">
           Affiliate Opt-in More Information Message
         </div>
       </div>
