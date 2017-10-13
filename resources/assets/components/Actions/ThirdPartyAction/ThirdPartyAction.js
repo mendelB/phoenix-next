856a5b2b1e685d604c0f6f4a7f03f1fa24a804bc
Diego Lorenzo <diego@diegolorenzo.com>
2017-10-13 16:35:07 -0400

    Updating to add separate ThirdPartyAction component and new Actions directory.

---
+++ resources/assets/components/Actions/ThirdPartyAction/ThirdPartyAction.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+import Markdown from '../../Markdown';
+import { Flex, FlexCell } from '../../Flex';
+import StepHeader from '../../ActionPage/StepHeader';
+
+const ThirdParyAction = (props) => {
+  const {
+    content, dynamicLink, dynamicUrlParams, hideStepNumber, stepIndex,
+    template, title, userId,
+  } = props;
+
+  const urlParams = dynamicUrlParams.map((param) => {
+    if (param === 'northstarId') {
+      return `northstarId=${userId}`;
+    }
+
+    return `${param}=${props[param]}`;
+  });
+
+  const contentLink = dynamicLink.indexOf('?') === -1 ?
+    `${dynamicLink}?${urlParams.join('&')}`
+    :
+    `${dynamicLink}&${urlParams.join('&')}`;
+
+  return (
+    <FlexCell width="full">
+      <div className={classnames('action-step')}>
+        <Flex>
+          <StepHeader
+            title={title}
+            step={stepIndex}
+            template={template}
+            hideStepNumber={hideStepNumber}
+          />
+          { content ?
+            <FlexCell width="two-thirds">
+              <Markdown>{ content.replace(/:::[a-zA-Z]*:::/gi, contentLink) }</Markdown>
+            </FlexCell>
+            :
+            null
+          }
+        </Flex>
+      </div>
+    </FlexCell>
+  );
+};
+
+ThirdParyAction.propTypes = {
+  content: PropTypes.string,
+  dynamicLink: PropTypes.string,
+  dynamicUrlParams: PropTypes.arrayOf(PropTypes.string),
+  hideStepNumber: PropTypes.bool,
+  stepIndex: PropTypes.number.isRequired,
+  template: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  userId: PropTypes.string,
+};
+
+ThirdParyAction.defaultProps = {
+  content: null,
+  dynamicLink: null,
+  dynamicUrlParams: null,
+  hideStepNumber: false,
+  userId: null,
+};
+
+export default ThirdParyAction;
---
+++ resources/assets/components/Actions/ThirdPartyAction/index.js
@@ -0,0 +1,8 @@
+export ThirdPartyAction from './ThirdPartyAction';
+
+export ThirdPartyActionContainer from './ThirdPartyActionContainer';
+
+/**
+ * By default we prefer to export the container.
+ */
+export default from './ThirdPartyActionContainer';
