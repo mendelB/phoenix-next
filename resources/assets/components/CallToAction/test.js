72d89acf1d662f81c7d2adfa99d51a11cfb2eb2e
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-10-17 15:33:22 -0400

    adding semi-colon, removing old import

--- resources/assets/components/CallToAction/test.js
+++ resources/assets/components/CallToAction/test.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 import CallToAction from './CallToAction';
 
-const component = shallow(<CallToAction className="sample-class" onClick={() => {}} />)
+const component = shallow(<CallToAction className="sample-class" onClick={() => {}} />);
 
 test('CallToAction snapshot test', () => {
   const tree = shallowToJson(component);
