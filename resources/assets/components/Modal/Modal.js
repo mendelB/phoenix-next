64a94413dbc0291e91c20804247e51667437be2d
Joe Kent <thedeadlybutter@gmail.com>
2017-10-19 11:54:49 -0400

    Adds modal tracking for Puck (#461)

--- resources/assets/components/Modal/Modal.js
+++ resources/assets/components/Modal/Modal.js
@@ -8,6 +8,15 @@ class Modal extends React.Component {
     super();
 
     this.handleOverlayClick = this.handleOverlayClick.bind(this);
+    this.trackModalState = this.trackModalState.bind(this);
+  }
+
+  componentDidMount() {
+    this.trackModalState();
+  }
+
+  componentDidUpdate() {
+    this.trackModalState();
   }
 
   handleOverlayClick(event) {
@@ -16,6 +25,14 @@ class Modal extends React.Component {
     }
 
     this.props.closeModal();
+  }
+
+  trackModalState() {
+    const { modalType, shouldShowModal, trackEvent } = this.props;
+
+    if (shouldShowModal) {
+      trackEvent('open modal', { modalType });
+    }
   }
 
   render() {
@@ -35,13 +52,16 @@ class Modal extends React.Component {
 }
 
 Modal.propTypes = {
-  shouldShowModal: PropTypes.bool.isRequired,
-  closeModal: PropTypes.func.isRequired,
   children: PropTypes.node,
+  closeModal: PropTypes.func.isRequired,
+  modalType: PropTypes.string,
+  shouldShowModal: PropTypes.bool.isRequired,
+  trackEvent: PropTypes.func.isRequired,
 };
 
 Modal.defaultProps = {
   children: null,
+  modalType: null,
 };
 
 export default Modal;
--- resources/assets/components/Modal/containers/ModalContainer.js
+++ resources/assets/components/Modal/containers/ModalContainer.js
@@ -1,13 +1,23 @@
 import { connect } from 'react-redux';
+import { PuckConnector } from '@dosomething/puck-client';
 import { closeModal } from '../../../actions/modal';
 import Modal from '../Modal';
 
 const mapStateToProps = state => ({
   shouldShowModal: state.modal.shouldShowModal,
+  modalType: state.modal.modalType,
 });
 
 const actionCreators = {
   closeModal,
 };
 
-export default connect(mapStateToProps, actionCreators)(Modal);
+const mapPropsToEvents = trackEvent => ({
+  closeModal: ({ modalType }) => (
+    trackEvent('close modal', { modalType })
+  ),
+});
+
+export default connect(mapStateToProps, actionCreators)(
+  PuckConnector(Modal, mapPropsToEvents),
+);
