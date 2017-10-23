2b78a01b7ebe138680c78812e4c62bf51302e544
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-10-23 15:49:29 -0400

    formatting contentful image urls within provided markdown string

--- resources/assets/components/Markdown/index.js
+++ resources/assets/components/Markdown/index.js
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import classnames from 'classnames';
-import { markdown } from '../../helpers';
+import { markdown, contentfulImageUrl } from '../../helpers';
 
 import './markdown.scss';
 
+const formatImageUrl = url => (contentfulImageUrl(url, '1000', '700', 'fill'));
+const formatImageUrls = string => (string.replace(/\/\/images\.contentful\.com.+\.jpg/g, formatImageUrl));
+
 const Markdown = ({ className = null, children }) => (
-  <div className={classnames('markdown', 'with-lists', className)} dangerouslySetInnerHTML={markdown(children)} /> // eslint-disable-line react/no-danger
+  <div className={classnames('markdown', 'with-lists', className)} dangerouslySetInnerHTML={markdown(formatImageUrls(children))} /> // eslint-disable-line react/no-danger
 );
 
 Markdown.propTypes = {
