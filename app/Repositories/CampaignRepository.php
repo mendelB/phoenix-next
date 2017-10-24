f6bfa6025c64fb07a27e888de46f36324726c815
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-10-24 16:19:13 -0400

    If available - using cached version of campaign instad of querying Contentful API

--- app/Repositories/CampaignRepository.php
+++ app/Repositories/CampaignRepository.php
@@ -47,21 +47,36 @@ class CampaignRepository
      */
     public function findBySlug($slug)
     {
-        $query = (new Query)
+        if (Cache::has($slug))
+        {
+            $cachedCampaignJson = Cache::get($slug);
+
+            // This turns the JSON into a Contentful Dynamic Entry
+            // so we can create a campaign entity out of the cached data
+            $revivedContentfulEntry = $this->client->reviveJson($cachedCampaignJson);
+
+            $campaign = new Campaign($revivedContentfulEntry);
+        }
+        else {
+            $query = (new Query)
             ->setContentType('campaign')
             ->where('fields.slug', $slug)
             ->setInclude(3)
             ->setLimit(1);
 
-        $campaigns = $this->makeRequest($query);
+            $campaigns = $this->makeRequest($query);
 
-        if (! $campaigns->count()) {
-            throw new ModelNotFoundException;
+            if (! $campaigns->count()) {
+                throw new ModelNotFoundException;
+            }
+
+            $expiresAt = Carbon::now()->addMinutes(15);
+
+            Cache::add($slug, json_encode($campaigns[0]), $expiresAt);
+
+            $campaign = new Campaign($campaigns[0]);
         }
-        $expiresAt = Carbon::now()->addMinutes(15);
-
-        return new Campaign($campaigns[0]);
-        Cache::add($slug, json_encode($campaigns[0]), $expiresAt);
+        return $campaign;
     }
 
     /**
