68bbf333a2b22c58abc1cb9931f3380c1c529324
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-11-06 13:58:49 -0500

    getting fields from update from state when prop is not present

--- resources/assets/components/CampaignUpdateBlock/CampaignUpdateBlockContainer.js
+++ resources/assets/components/CampaignUpdateBlock/CampaignUpdateBlockContainer.js
@@ -1,15 +1,22 @@
 /* global window */
 
 import { connect } from 'react-redux';
+import { find } from 'lodash';
 import CampaignUpdateBlock from './CampaignUpdateBlock';
 import { makeShareLink } from '../../helpers';
 
-const mapStateToProps = (state, props) => ({
-  shareLink: makeShareLink('campaigns', {
-    domain: window.location.origin,
-    slug: state.campaign.slug,
-    key: props.id,
-  }),
-});
+const mapStateToProps = (state, props) => {
+  const campaignUpdateFields = props.fields ||
+    find(state.campaign.activityFeed, { id: props.id }).fields;
+
+  return {
+    fields: campaignUpdateFields,
+    shareLink: makeShareLink('campaigns', {
+      domain: window.location.origin,
+      slug: state.campaign.slug,
+      key: props.id,
+    }),
+  };
+};
 
 export default connect(mapStateToProps)(CampaignUpdateBlock);
