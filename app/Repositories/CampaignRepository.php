a9f7abfaeb888b43ca4576d68264feaa9cfe8478
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-11-08 10:58:39 -0500

    Revert "adding clearFromCache param to optionally clear campaign from cache"
    This reverts commit c31377f4ddd9cd2efe88e74f6bb1eaadbae7a2c4.

--- app/Repositories/CampaignRepository.php
+++ app/Repositories/CampaignRepository.php
@@ -43,12 +43,8 @@ class CampaignRepository
      * @return stdClass
      * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
      */
-    public function findBySlug($slug, $clearFromCache = false)
+    public function findBySlug($slug)
     {
-        if ($clearFromCache) {
-            app('cache')->forget('campaign_'.$slug);
-        }
-
         $skipCache = ! config('services.contentful.cache');
 
         $flattenedCampaign = remember('campaign_'.$slug, 15, function () use ($slug) {
